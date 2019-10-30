79d0c0c2670b2f1e47124b0d29c0c12435c8dd1b
Paul Kitonyi <paulmucimah@gmail.com>
2019-10-30 18:31:03 +0300

    Add links on user dashboard

--- src/user/userDashBoard.js
+++ src/user/userDashBoard.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from "../auth";
+import {Link} from 'react-router-dom';
 
 const Dashboard = () => {
 
     const {user: { name, email, role}} = isAuthenticated()
 
-    return (
-        <Layout title="Dashbord" description="User Dashboard" className="container">
+    const userLinks = () => {
+        return (
+            <div className="card">
+                 <h4 className="card-header">User Links</h4>
+                 <ul className="list-group-item">
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/cart">My Cart</Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/profile/update">Update Profile</Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+    const userInfo = () => {
+        return (
             <div className="card mb-5">
                 <h3 className="card-header">User Information</h3>
                 <ul className="list-group-item">
@@ -16,11 +33,29 @@ const Dashboard = () => {
                     <li className="list-group-item">{role === 1 ? 'Admin' : 'Registered User'} </li>
                 </ul>
             </div>
+        )
+    }
+
+    const purchaseHistory = () => {
+        return (
             <div className="card mb-5">
                 <h3 className="card-header">Purchase History</h3>
                 <ul className="list-group">
                     <li className="list-group-item">history</li>
                 </ul>
+            </div>
+        )
+    }
+    return (
+        <Layout title="Dashbord" description={`G'day ${name}!`} className="container">
+            <div className="row">
+                <div className="col-3">
+                    {userLinks()}
+                </div>
+                <div className="col-9">
+                    {userInfo()}
+                    {purchaseHistory()}
+                </div>
             </div>
         </Layout>
     )
