5b2f48489d9c2c962d167d3aefc5f26405ac25cf
Paul Kitonyi <paulmucimah@gmail.com>
2019-10-30 16:42:26 +0300

    Add signup signin signout link behaviors

--- src/auth/index.js
+++ src/auth/index.js
@@ -53,6 +53,17 @@ export const signout = (next) => {
     }
 }
 
+export const isAuthenticated = () => {
+    if(typeof window == 'undefined') {
+        return false
+    }
+    if(localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'))
+    }else {
+        return false
+    }
+}
+
 
 
 
