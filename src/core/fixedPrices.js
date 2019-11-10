198c48482b5d61de31a7eef7cbe1ae03d666314d
Paul Kitonyi <paulmucimah@gmail.com>
2019-11-10 07:35:15 +0300

    Add filter by price && filter by product

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "0/= to 1,000/=",
+        array: [0, 999]
+    },
+    {
+        _id: 2,
+        name: "1000/= to 2,000/=",
+        array: [1000, 2999]
+    },
+    {
+        _id: 3,
+        name: "2000/= to 3,000/=",
+        array: [3000, 3999]
+    },
+    {
+        _id: 4,
+        name: "3000/= to 4,000/=",
+        array: [4000, 4999]
+    },
+    {
+        _id: 5,
+        name: "More than 4,000/=",
+        array: [5000, 100000]
+    }
+]
