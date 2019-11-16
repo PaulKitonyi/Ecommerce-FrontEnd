c7df393b164ac589dc5727b724a7469712d94a81
Paul Kitonyi <paulmucimah@gmail.com>
2019-11-16 10:25:15 +0300

    Add search component with functionality

--- src/core/Search.js
+++ src/core/Search.js
@@ -48,13 +48,35 @@ const Search = () => {
         setData({...data, [name]: event.target.value, searched: false});
     }
 
+    const searchMessage = (searched, results) => {
+        if(searched && results.length > 0) {
+            return results.length === 1 ? `Found ${results.length} product` : `Found ${results.length} products`
+        }
+        if(searched && results.length < 1) {
+            return `No products found.`
+        } 
+    }
+
+    const searchedProducts = (results = []) => {
+        return (
+            <div>
+                <h2 className="mt-4 mb-4">
+                    {searchMessage(searched, results)}
+                </h2>
+                <div className="row">
+                    {results.map((product, i) => (<Card key = {i} product = {product}/>))}
+                </div>
+            </div>
+        )
+    }
+
     const searchForm = () => (
         <form onSubmit={searchSubmit}>
             <span className="input-group-text">
                 <div className="input-group input-group-lg">
                     <div className="input-group-prepend">
                         <select className="btn mr-2" onChange={handleChange("category")}>
-                            <option value="All">Pick Category</option>
+                            <option value="All">All</option>
                             {categories.map((c, i) => (<option key={i} value={c._id}>{c.name}</option>))}
                         </select>
                     </div>
@@ -74,7 +96,9 @@ const Search = () => {
         <div>
             <div className="container mb-3">
                 {searchForm()}
-                {JSON.stringify(results)}
+            </div>
+            <div className="container-fluid mb-3">
+                {searchedProducts(results)}
             </div>
         </div>
     )
